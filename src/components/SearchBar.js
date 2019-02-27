f6553bab4047c3480850f74f659dad89a628740f
Max Goodman <max.goodman@flatironschool.com>
2019-02-27 09:37:50 -0500

    adding event handler to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
     render(){
-        return (<input />);
+        return (<input onChange={this.onInputChange}/>);
+    }
+
+    onInputChange(e){
+        e.preventDefault();
+
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 
-
-
 const App = ()=>{ //constant never going to change
   return (<div><SearchBar /></div>);
 } 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+const API_KEY ="AIzaSyBd1LdIreiGycf4cOARvu-5Z86LOPGSwnI"
