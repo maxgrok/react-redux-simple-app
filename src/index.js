64a969c8225bf5fa46f71968566d36b2a8b2505c
Max Goodman <max.goodman@flatironschool.com>
2019-02-27 09:25:35 -0500

    adding changes to remote

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@
 //take this components generated html and put it on the page (in the DOM)
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 const API_KEY = "AIzaSyAYsUdlZGWe8rnCRKVHl-TXj19tAdQQB3k";
 
 const App = ()=>{ //constant never going to change
-  return (<div>Hi!</div>);
+  return (<div><SearchBar /></div>);
 } 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
