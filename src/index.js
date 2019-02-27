f178cb3f25edff4d019213aa93db412673a446ed
Max Goodman <max.goodman@flatironschool.com>
2019-02-27 09:30:48 -0500

    editing out api_key

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 
-const API_KEY = "AIzaSyAYsUdlZGWe8rnCRKVHl-TXj19tAdQQB3k";
+
 
 const App = ()=>{ //constant never going to change
   return (<div><SearchBar /></div>);
