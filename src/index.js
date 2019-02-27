2bdbebe262fcc150ccff36de1e7896098ef3abbb
Max Goodman <max.goodman@flatironschool.com>
2019-02-27 09:16:28 -0500

    adding components and youtube-api-search package

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//create some new component. this component should produce HTML
+
+//take this components generated html and put it on the page (in the DOM)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = "AIzaSyAYsUdlZGWe8rnCRKVHl-TXj19tAdQQB3k";
+
+const App = ()=>{ //constant never going to change
+  return (<div>Hi!</div>);
+} 
+
+ReactDOM.render(<App />, document.querySelector('.container'));
